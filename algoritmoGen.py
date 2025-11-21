a3c2260b1ec0c972f286539e97a6044474b16859
Emerson444 <132405708+Emerson444@users.noreply.github.com>
2025-11-21 14:32:51 -0300

    GA complement

--- algoritmoGen.py
+++ algoritmoGen.py
@@ -38,3 +38,33 @@ def read_tsp_file(tsp_text: str) -> dict[Dict[int, Tuple[float, float]], str]:
             return coorde, edge_weight_type
         
         
+
+        def att_distance(a: int, b: int, coorde: dict) -> float:
+            (x1, y1) = coorde[a]
+            (x2, y2) = coorde[b]
+            dx = x1 - x2
+            dy = y1 - y2
+            rij = math.sqrt((dx * dx + dy *dy)/10.0)
+            dij = math.ceil(rij)
+            return int(dij)
+        
+        def building_distance_matrix(coorde: dict, edge_weight_type: str):
+            n = len(coorde)
+            nodes = list(coorde.keys)
+            att_distance = {i: {} for i in nodes}
+            for i in nodes:
+                for j in nodes:
+                    if i == j:
+                        att_distance[i][j] = 0
+                    else:
+                        if edge_weight_type and edge_weight_type.upper().startswith("ATT"):
+                            math.dist[i][j] = att_distance(i, j, coorde)
+                        else:
+                            dx = coorde[i][0] - coorde[j][0]
+                            dy = coorde[i][1] - coorde[j][1]
+                            math.dist[i][j] = int(around(math.hypot(dx, dy)))
+
+            return math.dist
+        
+        
+
